0b10f78c9e0b642c4c38a5f47ce0d4015ef503de
Alex Wang <alexw668@gmail.com>
2011-07-29 14:07:32 -0700

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RpmLogger::Application.config.secret_token = 'a8ab6b8517b4a94711a44319ba9b96b8655b1725fac0164e6e780d6cd9b851c1dda8291515c36b80a599b7c06d6ece8a70ccddba15bc4922d0522b0955efc837'
